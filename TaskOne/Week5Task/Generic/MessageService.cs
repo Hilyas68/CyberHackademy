43ef8bc64b068727c3a86fd3a5e11918b9778f29
Hcode <htope68@gmail.com>
2018-02-16 16:16:02 +0100

    Updated Week 5 Task

--- TaskOne/Week5Task/Generic/MessageService.cs
+++ TaskOne/Week5Task/Generic/MessageService.cs
@@ -6,9 +6,17 @@ using System.Threading.Tasks;
 
 namespace TaskOne.Week5Task.Generic
 {
-    class MessageService<T>
+    class MessageService<T> : INotificationService<T>
     {
+        public void SendBulkMessage(string[] recipients, string sender, string message)
+        {
+            throw new NotImplementedException();
+        }
 
+        public void SendSingleMessage(string recipient, string sender, string mesage)
+        {
+            throw new NotImplementedException();
+        }
     }
 
     interface INotificationService<T>
